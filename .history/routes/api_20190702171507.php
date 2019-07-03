5f31ad76accd4169c4aceef53137ce96fa5606ce
Munish <genius4job@gmail.com>
2019-07-03 18:40:06 +0530

    Registration Passport

--- .history/routes/api_20190702171507.php
+++ .history/routes/api_20190702171507.php
@@ -13,15 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group([
-    'prefix' => 'auth'
-], function () {
-    Route::post('login', 'Auth\AuthController@login')->name('login');
-    Route::post('register', 'Auth\AuthController@register');
-    Route::group([
-      'middleware' => 'auth:api'
-    ], function() {
-        Route::get('logout', 'Auth\AuthController@logout');
-        Route::get('user', 'Auth\AuthController@user');
-    });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
